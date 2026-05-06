c146cf241341a7043892c570bcc0b138c11f4b5b
Aman Vishwakarma <aman.vishwakarma.dev@gmail.com>
2026-05-06 22:25:42 +0530

    fix: add react-native-url-polyfill to resolve Hermes URL getter error

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-url-polyfill/auto'
 import React, { useEffect, useRef, useState } from 'react'
 import { AppState, type AppStateStatus } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
